c25da75168f1c7dd353db59d2e3da3959d340af0
ericxjzhu <68457752+ericxjzhu@users.noreply.github.com>
2021-02-14 23:01:15 +1100

    Added more code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         int studentAge = 19;
         double atar = 99.95;
-        char firstInitial = 'E';
-        char lastInitial = 'Z';
+        String firstName = "Eric";
+        String lastName = "Zhu";
+        char firstInitial = firstName.charAt(0);
+        char lastInitial = lastName.charAt(0);
         boolean hasPerfectAttendance = true;
-
-        System.out.println(firstInitial + "" + lastInitial + " is " + studentAge + " years old and has " + atar + " ATAR.");
+        System.out.println(firstName + " " + lastName + " is a great person.");
+        System.out.println(firstInitial + "" + lastInitial + " is " + studentAge + " years old and has an ATAR of " + atar + ".");
         if (hasPerfectAttendance) {
             System.out.println("They also have perfect attendance.");
         }
         else {
             System.out.println("They are twats and do not have perfect attendance.");
         }
-
+        System.out.println("Do you want to update your age?");
+        Scanner input = new Scanner(System.in);
+        studentAge = input.nextInt();
+        System.out.println(firstName + " " + lastName + " is now " + studentAge);
     }
 }
