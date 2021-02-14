ab1c524eaff64b07471cd222764272084a4ad644
ericxjzhu <68457752+ericxjzhu@users.noreply.github.com>
2021-02-14 22:30:14 +1100

    Addded Code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 19;
+        double atar = 99.95;
+        char firstInitial = 'E';
+        char lastInitial = 'Z';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(firstInitial + "" + lastInitial + " is " + studentAge + " years old and has " + atar + " ATAR.");
+        if (hasPerfectAttendance) {
+            System.out.println("They also have perfect attendance.");
+        }
+        else {
+            System.out.println("They are twats and do not have perfect attendance.");
+        }
+
     }
 }
